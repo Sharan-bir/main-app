e9761793ca05a51f8c7b1190e5add77a3c6cc909
sharanbir <birsharan105@gmail.com>
2024-08-12 17:05:14 +0530

    simple template

---
+++ interfaces/signup.js
@@ -0,0 +1,17 @@
+import { View, Text , ScrollView, Image, Alert} from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+import React,{useState} from 'react';
+
+const signup = () => {
+  return (
+    <SafeAreaView>
+        <ScrollView>
+            <View>
+                <Text>signup</Text>
+            </View>
+        </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default signup;
