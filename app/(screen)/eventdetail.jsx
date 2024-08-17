2677a77fe989b23df6411287bcf03613b097c599
sharanbir <birsharan105@gmail.com>
2024-08-17 10:44:35 +0530

    added screen folder

---
+++ app/(screen)/eventdetail.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const eventdetail = () => {
+  return (
+    <View>
+      <Text>eventdetail</Text>
+    </View>
+  )
+}
+
+export default eventdetail
---
+++ app/(screen)/eventitem.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const eventitem = () => {
+  return (
+    <View>
+      <Text>eventitem</Text>
+    </View>
+  )
+}
+
+export default eventitem
