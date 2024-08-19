4fe8bc16d2ac15f8f42d88babc11c15656416402
Divineleo2001 <divineleo20@gmail.com>
2024-08-19 15:16:20 +0530

    initial root directory prior to tailwind

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from 'react'
+import { Link } from 'expo-router';
 
 const SignIn = () => {
   return (
     <SafeAreaView>
       <Text>SignIn</Text>
+      <Link
+        href="/(root)/(tabs)/home"
+      >
+        <Text>Go to home screen!</Text>
+      </Link>
     </SafeAreaView>
   )
 }
