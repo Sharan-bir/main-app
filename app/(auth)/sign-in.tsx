eae340228f32d484e02045b6fbe6ad017bf31b2e
Divineleo20 <divineleo20@gmail.com>
2024-08-19 12:37:25 +0530

    added the initial starter

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignIn</Text>
+    </SafeAreaView>
+  )
+}
+
+export default SignIn
